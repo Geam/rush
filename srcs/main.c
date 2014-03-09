5850ea9733c94a85d19d20a7e5a17fb1bc15a2bc
mdelage <mdelage@student.42.fr>
2014-03-09 21:55:30 +0100

    correction d'une fuite

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mdelage <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/08 11:04:36 by mdelage           #+#    #+#             */
-/*   Updated: 2014/03/09 21:34:43 by mdelage          ###   ########.fr       */
+/*   Updated: 2014/03/09 21:52:37 by mdelage          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int		main(int ac, char **av)
 		if (data.board != NULL)
 		{
 			ft_start(&data);
-			free(data.board);
+			ft_free_tab(&(data.board));
 		}
 		else
 			ft_putendl_fd("Error : wrong board game size.", 2);
