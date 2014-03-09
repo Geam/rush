fe54ecedfa9cf357d8bc18e53509e8fb275e1c6b
mdelage <mdelage@student.42.fr>
2014-03-09 17:40:12 +0100

    presque fonctionnel

--- srcs/ft_eval.c
+++ srcs/ft_eval.c
@@ -6,38 +6,68 @@
 /*   By: mdelage <mdelage@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/09 15:55:41 by mdelage           #+#    #+#             */
-/*   Updated: 2014/03/09 15:57:09 by mdelage          ###   ########.fr       */
+/*   Updated: 2014/03/09 17:39:11 by mdelage          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_struct_data.h"
 
+int		ft_how_many_raw(t_data *data, int player, int coor[2], int dir[2], int len)
+{
+	if ((data->board)[coor[0]][coor[1]] = player)
+	{
+		if (len == 0)
+			return (1);
+		else if (coor[0] + dir[0] < data->x && coor[1] + dir[1] < data->y)
+		{
+			coor[0] += dir[0];
+			coor[1] += dir[1];
+			return (ft_how_many_raw(data, player, coor, dir, len - 1));
+		}
+		else
+			return (0);
+	}
+}
+
 void	ft_raw(t_data *data, int raw[2])
 {
-	/* compter le nombre de ligne de 2 ou 3 pions de chaque joeur.*/
+	/* compter le nombre de ligne de 3 pions de chaque joeur.*/
 	/* joueur1 -> raw[0]; joueur2 -> raw[1] */
-	raw[0] = 0;
-	raw[1] = 0;
+	int		player;
+	int		coor[2];
+	int		dir[2];
+
+	player = 0;
+	while (player < 2)
+	{
+		raw[player] = 0;
+		coor[0] = 0;
+		while (coor[0] < data->x)
+		{
+			coor[1] = 0;
+			while (coor[1] < data->y)
+			{
+				dir[0] = 1;
+				dir[1] = 0;
+				raw[player] += ft_how_many_raw(data, player + 1, coor, dir, 0);
+				dir[1] = 1;
+				raw[player] += ft_how_many_raw(data, player + 1, coor, dir, 0);
+				dir[0] = 0;
+				raw[player] += ft_how_many_raw(data, player + 1, coor, dir, 0);
+				(coor[1])++;
+			}
+			(coor[0])++;
+		}
+		player++;
+	}
 	return ;
 }
 
 int		ft_eval(t_data *data)
 {
 	/* determine le "poid" du plateau */
-	int		piece;
 	int		raw[2];
 
-	piece = ft_how_many_piece(); /* retourne nb piece sur plateau */
-	if (/* partie finie */)
-	{
-		if (/* IA gagne */)
-			return (1000 - piece);
-		else
-			return (-1000 + piece);
-	}
-	else
-	{
-		ft_raw(data, raw);
-		return (raw[0] - raw[1]);
-	}
+	ft_raw(data, raw);
+	return (raw[0] - raw[1]);
 }
--- srcs/ft_winner.c
+++ srcs/ft_winner.c
@@ -6,11 +6,31 @@
 /*   By: mdelage <mdelage@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/09 15:56:23 by mdelage           #+#    #+#             */
-/*   Updated: 2014/03/09 15:57:16 by mdelage          ###   ########.fr       */
+/*   Updated: 2014/03/09 17:38:32 by mdelage          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_struct_data.h"
+
+int		ft_how_many_piece(t_data *data)
+{
+	int		i;
+	int		j;
+	int		nb_piece;
+
+	i = 0;
+	nb_piece = 0;
+	while (i < data->x)
+	{
+		j = 0;
+		while (j < data->y)
+		{
+			if ((data->board)[i][j])
+				nb_piece++;
+		}
+	}
+	return (nb_piece);
+}
 
 int		ft_winner(t_data *data)
 {
@@ -19,7 +39,9 @@ int		ft_winner(t_data *data)
 	/* 1 = joueur 1 gagne */
 	/* 2 = joueur 2 gagne */
 	/* 3 = match nul */
+	int		nb_piece;
 
+	nb_piece = ft_how_many_piece(data);
 	if ()
 		return (1);
 	else if ()
